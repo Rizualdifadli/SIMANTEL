fbc79b3025fa074d9a6064f8c66ef1f95f52b9a4
Rizualdi Fadli <59524130+Rizualdifadli@users.noreply.github.com>
2020-08-10 11:12:58 +0700

    Update main.blade.php
    update tombol search baru

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -26,15 +26,49 @@
       </ul>
     </div>
 <section>
-  <nav>
-   <a class="btn btn-primary" href="{{url('/login')}}" role="button">LOGIN</a>
-   <form class="form-inline">
-      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-    </form>
-  </nav>
+<nav>
+<a class="btn btn-primary" href="{{url('/login')}}" role="button">LOGIN</a>
+      <div class="menu-icon">
+      <span></span></div>
+      <div class="nav-items">
+      </div>
+      <div class="search-icon">
+      <span class="fas fa-search"></span></div>
+      <div class="cancel-icon">
+      <span class="fas fa-times"></span></div>
+      <form action="#">
+        <input type="search" class="search-data" placeholder="Search" required>
+        <button type="submit" class="fas fa-search"></button>
+      </form>
+      
+</nav>
   @yield('container')
 </section>
-
+<script>
+    const menuBtn = document.querySelector(".menu-icon span");
+    const searchBtn = document.querySelector(".search-icon");
+    const cancelBtn = document.querySelector(".cancel-icon");
+    const items = document.querySelector(".nav-items");
+    const form = document.querySelector("form");
+    menuBtn.onclick = ()=>{
+      items.classList.add("active");
+      menuBtn.classList.add("hide");
+      searchBtn.classList.add("hide");
+      cancelBtn.classList.add("show");
+    }
+    cancelBtn.onclick = ()=>{
+      items.classList.remove("active");
+      menuBtn.classList.remove("hide");
+      searchBtn.classList.remove("hide");
+      cancelBtn.classList.remove("show");
+      form.classList.remove("active");
+      cancelBtn.style.color = "#ff3d00";
+    }
+    searchBtn.onclick = ()=>{
+      form.classList.add("active");
+      searchBtn.classList.add("hide");
+      cancelBtn.classList.add("show");
+    }
+  </script>
   </body>
 </html>
