fc0d55294502d88332fd5cd74bb81ec3a550c088
Qindilsyah Abrar Hanif <37163851+hanifbadar@users.noreply.github.com>
2020-09-02 09:59:27 +0700

    assign role otomatis pada user baru

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -64,9 +64,13 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
-        return User::create([
+        $user= User::create([
             'name' => $data['name'],
             'password' => Hash::make($data['password']),
         ]);
+        $role=Role::select('id')->where('name','user')->first();
+        $user=roles()->attach($role);
+
+        Return $user;
     }
 }
