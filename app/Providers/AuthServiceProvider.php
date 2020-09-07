980afdca96fc9318df2ff1d5e7996ffaec1aa2d5
Qindilsyah Abrar Hanif <37163851+hanifbadar@users.noreply.github.com>
2020-09-07 15:10:31 +0700

    seeding role admin per kecamatan

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -37,6 +37,38 @@ class AuthServiceProvider extends ServiceProvider
             return $user->hasRole(['admin','adminb']);
         });
 
+        Gate::define('manage-tower-mersam',function($user){
+            return $user->hasRole(['admin','adminb']);
+        });
+
+        Gate::define('manage-tower-bulian',function($user){
+            return $user->hasRole(['admin','adminb']);
+        });
+
+        Gate::define('manage-tower-tembesi',function($user){
+            return $user->hasRole(['admin','adminb']);
+        });
+
+        Gate::define('manage-tower-batin',function($user){
+            return $user->hasRole(['admin','adminb']);
+        });
+
+        Gate::define('manage-tower-pemayung',function($user){
+            return $user->hasRole(['admin','adminb']);
+        });
+
+        Gate::define('manage-tower-seboulu',function($user){
+            return $user->hasRole(['admin','adminb']);
+        });
+
+        Gate::define('manage-tower-seboilir',function($user){
+            return $user->hasRole(['admin','adminb']);
+        });
+
+        Gate::define('manage-tower-bajubang',function($user){
+            return $user->hasRole(['admin','adminb']);
+        });
+
         Gate::define('register-users',function($user){
             return $user->hasRole(['admin','adminb']);
         });
--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -17,21 +17,56 @@ class UsersTableSeeder extends Seeder
         User::truncate();
         DB::table('role_user')->truncate();
         $adminRole = Role::where('name','admin')->first();
-        $adminbRole = Role::where('name','adminb')->first();
+        $adminmersamRole = Role::where('name','adminmersam')->first();
+        $adminbulianRole = Role::where('name','adminbulian')->first();
+        $adminbajubangRole = Role::where('name','adminbajubang')->first();
+        $adminbatinRole = Role::where('name','adminbatin')->first();
+        $adminpemayungRole = Role::where('name','adminpemayung')->first();
+        $admintembesiRole = Role::where('name','admintembesi')->first();
+        $adminsebouluRole = Role::where('name','adminseboulu')->first();
+        $adminseboilirRole = Role::where('name','adminseboilir')->first();
         $userRole = Role::where('name','user')->first();
 
         $admin=User::create(['name'=>'Admin User',
         	'password'=>Hash::make('password')
     ]);
-         $adminb=User::create(['name'=>'Adminb User',
+         $adminmersam=User::create(['name'=>'Admin Mersam',
         	'password'=>Hash::make('password')
     ]);
-          $user=User::create(['name'=>'User',
+          $adminbulian=User::create(['name'=>'Admin Bulian',
         	'password'=>Hash::make('password')
+    ]);
+          $adminbajubang=User::create(['name'=>'Admin Bajubang',
+          'password'=>Hash::make('password')
+    ]);
+          $adminbatin=User::create(['name'=>'Admin Batin',
+          'password'=>Hash::make('password')
+    ]);
+          $adminpemayung=User::create(['name'=>'Admin Pemayung',
+          'password'=>Hash::make('password')
+    ]);
+          $admintembesi=User::create(['name'=>'Admin Tembesi',
+          'password'=>Hash::make('password')
+    ]);
+          $adminseboulu=User::create(['name'=>'Admin Sebo Ulu',
+          'password'=>Hash::make('password')
+    ]);
+          $adminseboilir=User::create(['name'=>'Admin Sebo Ilir',
+          'password'=>Hash::make('password')
+    ]);
+          $user=User::create(['name'=>'User',
+          'password'=>Hash::make('password')
     ]);
 
     $admin->roles()->attach($adminRole);
-    $adminb->roles()->attach($adminbRole);
+    $adminmersam->roles()->attach($adminmersamRole);
+    $adminbulian->roles()->attach($adminbulianRole);
+    $adminbajubang->roles()->attach($adminbajubangRole);
+    $adminbatin->roles()->attach($adminbatinRole);
+    $adminpemayung->roles()->attach($adminpemayungRole);
+    $admintembesi->roles()->attach($admintembesiRole);
+    $adminseboulu->roles()->attach($adminsebouluRole);
+    $adminseboilir->roles()->attach($adminseboilirRole);
     $user->roles()->attach($userRole);
     }
 }
