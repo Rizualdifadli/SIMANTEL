3d1bec43e7ea2149f50d764cf105468902a758cb
rizualdi fadli <rizualdifadli2015@gmail.com>
2020-09-10 12:06:50 +0700

    edit create

--- storage/framework/views/fb4e41e82eabadfb4f50c30a41bb933392169814.php
+++ storage/framework/views/fb4e41e82eabadfb4f50c30a41bb933392169814.php
@@ -4,18 +4,17 @@
 
 <?php $__env->startSection('content'); ?>
 <link rel="stylesheet" href="/css/edit.css">
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
 <div class="jumbotron">
 <h3 class="text-center">FORM TAMBAH DATA TOWER KECAMATAN</h3>
 <form method="post" action="/kecamatanmersam_admin">
 <?php echo csrf_field(); ?>
 
-  <div class="form-group">
+  <div class="form-group col-md-12 ">
     <label for="exampleInputEmail1">TOWER</label>
     <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" name="tower_id" required>
   </div>
 
-  <div class="">
+  <div class="col-md-6">
       <label for="validationCustom04">KODE KECAMATAN</label>
       <select class="custom-select" id="validationCustom04" required name="kode_kecamatan">
       <option selected disabled value="">SILAHKAN PILIH KODE KECAMATAN</option>
@@ -32,51 +31,66 @@
         Please select a valid state.
       </div>
     </div>
-<div class="form-group">
+<div class="form-group col-md-12">
     <label for="exampleInputPassword1" >DESA</label>
     <input type="text" class="form-control" id="exampleInputEmail" aria-describedby="emailHelp" name="desa" >
   </div>
-  <div class="form-group">
+  <div class="form-group col-md-12">
     <label for="exampleInputPassword1">KOORDINAT</label>
     <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" name="koordinat">
   </div>
-  <div class="form-group">
+  <div class="form-group col-md-12">
     <label for="exampleInputPassword1">KETINGGIAN</label>
     <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" name="ketinggian_meter">
   </div>
-  <div class="form-group">
+  <div class="form-group col-md-12">
     <label for="exampleInputPassword1">PEMILIK TANAH</label>
     <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" name="pemilik_tanah">
   </div>
-  <div class="form-group">
-    <label for="exampleInputPassword1">IZIN TOWER</label>
-    <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" name="izin_tower" >
+  <div class="form-group col-md-2">
+    <select class="custom-select" id="validationCustom04" name="izin_tower">
+      <option selected disabled value=""> IZIN TOWER</option>
+        <option>Yes</option>
+        <option>No</option>
+      </select>
   </div>
-  <div class="form-group">
-    <label for="exampleInputPassword1">LISTRIK</label>
-    <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" name="listrik">
+  <div class="form-group col-md-2">
+  <select class="custom-select" id="validationCustom04" name="listrik">
+      <option selected disabled value="">LISTRIK</option>
+        <option>Yes</option>
+        <option>No</option>
+    </select>
   </div>
-  <div class="form-group">
-    <label for="exampleInputPassword1">PAGAR TOWER</label>
-    <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" name="pagar_tower">
+  <div class="form-group col-md-2">
+  <select class="custom-select" id="validationCustom04" name="listrik">
+      <option selected disabled value="">PAGAR TOWER</option>
+        <option>Yes</option>
+        <option>No</option>
+    </select>
   </div>
-  <div class="form-group">
-    <label for="exampleInputPassword1">PAPAN NAMA TOWER</label>
-    <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" name="papan_nama">
+  <div class="form-group col-md-2">
+  <select class="custom-select" id="validationCustom04" name="papan_nama">
+      <option selected disabled value="">PAPAN NAMA TOWER</option>
+        <option>Yes</option>
+        <option>No</option>
+    </select>
   </div>
-  <div class="form-group">
+  <div class="form-group col-md-12">
     <label for="exampleInputPassword1">PETUGAS</label>
     <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" name="petugas">
   </div>
-  <div class="form-group">
+  <div class="form-group col-md-12">
     <label for="exampleInputPassword1">HP PETUGAS</label>
     <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" name="hp_petugas">
   </div>
-  <div class="form-group">
-    <label for="exampleInputPassword1">SHELTER DAN GENSET</label>
-    <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" name="shelter_genset" >
+  <div class="form-group col-md-2">
+  <select class="custom-select" id="validationCustom04" name="shelter_genset">
+      <option selected disabled value="">SHELTER DAN GENSET</option>
+        <option>Yes</option>
+        <option>No</option>
+    </select>
   </div>
-  <div class="custom-file col-md-3 mb-3">
+  <div class="custom-file form-group col-md-2">
     <input type="file" class="custom-file-input" id="validatedCustomFile" name="gambar" >
     <label class="custom-file-label" for="validatedCustomFile">Pilih Gambar</label>
     <div class="invalid-feedback">Example invalid custom file feedback</div>
