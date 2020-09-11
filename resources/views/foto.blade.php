fb94f1602cb5f436a0b421591fed41f5b1a18ac0
rizualdi fadli <rizualdifadli2015@gmail.com>
2020-09-11 11:23:43 +0700

    pppp

--- resources/views/foto.blade.php
+++ resources/views/foto.blade.php
@@ -5,11 +5,13 @@
 @section('content')
 <link rel="stylesheet" href="/css/gambar.css">
 <div class="jumbotron">
-<h3 class="text-center">FOTO TOWER KECAMATAN</h3>
+@foreach($data as $x)
+<h3 class="text-center">FOTO TOWER {{$x->tower_id}}</h3>
     @csrf
-    @foreach($data as $x)
+    
   <div class="form-group rounded mx-auto d-block">
     <img src=" {{asset( 'storage/'. $x->gambar) }}" class="rounded mx-auto d-block" alt="foto_tower">
+    <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis perferendis vitae dolor saepe unde suscipit ex eius obcaecati animi quam labore quas eos soluta, veritatis rerum quo in odio vero!</h3>
   </div>
   @endforeach
 
--- storage/framework/views/9a1249ad86d5cbec69cf119e67d6019a7347389f.php
+++ storage/framework/views/9a1249ad86d5cbec69cf119e67d6019a7347389f.php
@@ -5,11 +5,13 @@
 <?php $__env->startSection('content'); ?>
 <link rel="stylesheet" href="/css/gambar.css">
 <div class="jumbotron">
-<h3 class="text-center">FOTO TOWER KECAMATAN</h3>
+<?php $__currentLoopData = $data; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $x): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+<h3 class="text-center">FOTO TOWER <?php echo e($x->tower_id); ?></h3>
     <?php echo csrf_field(); ?>
-    <?php $__currentLoopData = $data; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $x): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+    
   <div class="form-group rounded mx-auto d-block">
     <img src=" <?php echo e(asset( 'storage/'. $x->gambar)); ?>" class="rounded mx-auto d-block" alt="foto_tower">
+    <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis perferendis vitae dolor saepe unde suscipit ex eius obcaecati animi quam labore quas eos soluta, veritatis rerum quo in odio vero!</h3>
   </div>
   <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
 
